49eadb2bfd33b08b08432bf4926194dbd91afe84
molteencreates <safwanbinhasan2017@gmail.com>
2025-05-10 18:39:24 +0400

    Update __init__.py

--- moderation/__init__.py
+++ moderation/__init__.py
@@ -1,1 +1,10 @@
+from typing import TYPE_CHECKING
 
+from .cog import Moderation
+
+if TYPE_CHECKING:
+    from ballsdex.core.bot import BallsDexBot
+
+
+async def setup(bot: "BallsDexBot"):
+    await bot.add_cog(Moderation(bot))
